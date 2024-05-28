dd5519fb2fa42399b6ddcff2e0214e6cf487808d
bhalajisivakumar123 <bhalajisivakumar@gmail.com>
2024-05-28 22:11:07 +0530

    Update scan.java

--- scan.java
+++ scan.java
@@ -13,7 +13,8 @@ public class scan {
 
             Scanner A = new Scanner(System.in);
             int e = A.nextInt();
-            String f = A.next().charAt(0);
+            String f = A.next();
+            char g = A.next().charAt(0);
                 
             System.out.println(e);
 
