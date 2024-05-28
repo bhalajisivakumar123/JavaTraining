ccd9a720a80055b8ac61a9544944fb4e50908254
bhalajisivakumar123 <bhalajisivakumar@gmail.com>
2024-05-28 22:06:22 +0530

    Add files via upload
    Scanner class - trials

---
+++ scan.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class scan {
+
+        public static void main(String[] args) {
+
+       /* boolean bool = true;
+        System.out.println("Hello and welcome!");
+
+        int a = 444444;
+        byte b = (byte)a;
+        System.out.println(b); */
+
+            Scanner A = new Scanner(System.in);
+            int e = A.nextInt();
+            String f = A.next();
+
+            System.out.println(e);
+
+
+        }
+}
+
